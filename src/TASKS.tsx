a98b43951bd0cdf99d95ecac2107626e94137829
tamga05 <tamga05@gmail.com>
2023-01-06 14:06:26 +0300

    added map method and finished markup

--- src/TASKS.tsx
+++ src/TASKS.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
 
 
-type StudentsType = Array<string>
+export type StudentsType = Array<string>
 
-type TasksType = Array<TaskType>
-
-type TaskType = {
+export type TaskType = {
     taskId: number,
     title: string,
     isDone: boolean
 }
 
-type DataType = {
+export type TasksType = Array<TaskType>
+
+export type DataType = {
     title: string,
     tasks: TasksType,
     students: StudentsType
 }
 
-type TASKSPropsType = {
+export type TASKSType = {
     data: DataType
 }
 
 
-export const TASKS = (props: TASKSPropsType) => {
+export const TASKS = (props: TASKSType) => {
     return (
         <div>
             <h3>{props.data.title}</h3>
-
-
+            <ul>
+                {props.data.tasks.map((el) => {
+                    return (
+                        <li>
+                            <span>{el.title}</span>
+                            <input type={'checkbox'} checked={el.isDone}/>
+                        </li>
+                    );
+                })}
+            </ul>
+            <ul>
+                <li>{props.data.students.map(el => {
+                    return (
+                        <li>{el}</li>
+                    );
+                })}
+                </li>
+            </ul>
         </div>
     );
 };
-
