f9f084bdb6ba8a4017b4a5649935b9917fc2ac23
tamga05 <tamga05@gmail.com>
2023-01-17 10:39:42 +0300

    coding and typing training

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
-import {Tasks} from './Tasks';
-
+import React from "react";
+import "./App.css";
+import { Tasks } from "./Tasks";
 
 // Hi Guys!
 // Let's reinforce our current session!
@@ -9,142 +8,141 @@ import {Tasks} from './Tasks';
 // -Don't forget to assign types to our data.
 
 export type TaskType = {
-    taskId: number
-    title: string
-    isDone: boolean
-}
+  taskId: number;
+  title: string;
+  isDone: boolean;
+};
 
 export type DataType = {
-    title: string
-    tasks: Array<TaskType>
-    students: Array<string>
-}
+  title: string;
+  tasks: Array<TaskType>;
+  students: Array<string>;
+};
 
 function App() {
-    const data1 = {
-        title: 'What to do',
-        tasks: [
-            {taskId: 1, title: 'HTML&CSS2', isDone: true},
-            {taskId: 2, title: 'JS2', isDone: true}
-        ],
-        students: [
-            'Jago Wormald1',
-            'Saul Milne2',
-            'Aariz Hester3',
-            'Dion Reeve4',
-            'Anisa Ortega5',
-            'Blade Cisneros6',
-            'Malaikah Phelps7',
-            'Zeeshan Gallagher8',
-            'Isobella Vo9',
-            'Rizwan Mathis10',
-            'Menaal Leach11',
-            'Kian Walton12',
-            'Orion Lamb13',
-            'Faizah Huynh14',
-            'Crystal Vaughan15',
-            'Vivien Hickman16',
-            'Stuart Lu17',
-            'Karol Davison18',
-            'Dario Burns19',
-            'Chloe Rich20',
-            'Martyna Felix',
-            'Nida Glass',
-            'Maeve Miles',
-            'Hasnain Puckett',
-            'Ayman Cano',
-            'Safwan Perry',
-            'Fox Kelly',
-            'Louise Barlow',
-            'Malaki Mcgill',
-            'Leanna Cline',
-            'Willard Hodge',
-            'Amelia Dorsey',
-            'Kiah Porter',
-            'Jeanne Daly',
-            'Mohsin Armstrong',
-            'Laurie Rangel',
-            'Princess Tierney',
-            'Kasim Kendall',
-            'Darryl Cope',
-            'Elysha Ray',
-            'Liyana Harris',
-            'Kashif Blackburn',
-            'Atif Zimmerman',
-            'Sila Hartley',
-            'Ralphie Hebert',
-        ]
-    };
-    const data2 = {
-        title: 'What to learn',
-        tasks: [
-            {taskId: 1, title: 'HTML&CSS', isDone: true},
-            {taskId: 2, title: 'JS', isDone: true}
-        ],
-        students: [
-            'Rick Kane',
-            'Finnlay Bentley',
-            'Samia North',
-            'Isaac Morton',
-            'Lily-Ann Clifford',
-            'Thalia Park',
-            'Sapphire Cruz',
-            'Cieran Vazquez',
-            'Anya Estes',
-            'Dominika Field',
-            'Rosanna Chung',
-            'Safiyah Davey',
-            'Ryley Beasley',
-            'Kalvin Trejo',
-            'Evie-Mae Farrell',
-            'Juliet Valencia',
-            'Astrid Austin',
-            'Lyle Montgomery',
-            'Nisha Mora',
-            'Kylie Callaghan',
-            'Star Wilks',
-            'Marissa Colley',
-            'Asa Fuller',
-            'Leigh Kemp',
-            'Avleen Dawson',
-            'Sammy Bonilla',
-            'Acacia Becker',
-            'Coral Shepherd',
-            'Melina Molina',
-            'Kiran Bailey',
-            'Clara Escobar',
-            'Alexandru Horn',
-            'Brandon-Lee Mercado',
-            'Elouise Weston',
-            'King Long',
-            'Kerri Searle',
-            'Kanye Hamer',
-            'Elwood Benitez',
-            'Mikail Whitaker',
-            'Bobby Hardy',
-            'Talha Ferry',
-            'Priscilla Landry',
-            'Olivia-Grace Cain',
-            'Kiaan Wallace',
-            'Wesley Padilla90',
-            'Ella-Grace Wooten91',
-            'Kaif Molloy92',
-            'Kamal Broadhurst93',
-            'Bianca Ferrell94',
-            'Micheal Talbot95',
-        ]
-    };
+  const data1 = {
+    title: "What to do",
+    tasks: [
+      { taskId: 1, title: "HTML&CSS2", isDone: true },
+      { taskId: 2, title: "JS2", isDone: true },
+    ],
+    students: [
+      "Jago Wormald1",
+      "Saul Milne2",
+      "Aariz Hester3",
+      "Dion Reeve4",
+      "Anisa Ortega5",
+      "Blade Cisneros6",
+      "Malaikah Phelps7",
+      "Zeeshan Gallagher8",
+      "Isobella Vo9",
+      "Rizwan Mathis10",
+      "Menaal Leach11",
+      "Kian Walton12",
+      "Orion Lamb13",
+      "Faizah Huynh14",
+      "Crystal Vaughan15",
+      "Vivien Hickman16",
+      "Stuart Lu17",
+      "Karol Davison18",
+      "Dario Burns19",
+      "Chloe Rich20",
+      "Martyna Felix",
+      "Nida Glass",
+      "Maeve Miles",
+      "Hasnain Puckett",
+      "Ayman Cano",
+      "Safwan Perry",
+      "Fox Kelly",
+      "Louise Barlow",
+      "Malaki Mcgill",
+      "Leanna Cline",
+      "Willard Hodge",
+      "Amelia Dorsey",
+      "Kiah Porter",
+      "Jeanne Daly",
+      "Mohsin Armstrong",
+      "Laurie Rangel",
+      "Princess Tierney",
+      "Kasim Kendall",
+      "Darryl Cope",
+      "Elysha Ray",
+      "Liyana Harris",
+      "Kashif Blackburn",
+      "Atif Zimmerman",
+      "Sila Hartley",
+      "Ralphie Hebert",
+    ],
+  };
+  const data2 = {
+    title: "What to learn",
+    tasks: [
+      { taskId: 1, title: "HTML&CSS", isDone: true },
+      { taskId: 2, title: "JS", isDone: true },
+    ],
+    students: [
+      "Rick Kane",
+      "Finnlay Bentley",
+      "Samia North",
+      "Isaac Morton",
+      "Lily-Ann Clifford",
+      "Thalia Park",
+      "Sapphire Cruz",
+      "Cieran Vazquez",
+      "Anya Estes",
+      "Dominika Field",
+      "Rosanna Chung",
+      "Safiyah Davey",
+      "Ryley Beasley",
+      "Kalvin Trejo",
+      "Evie-Mae Farrell",
+      "Juliet Valencia",
+      "Astrid Austin",
+      "Lyle Montgomery",
+      "Nisha Mora",
+      "Kylie Callaghan",
+      "Star Wilks",
+      "Marissa Colley",
+      "Asa Fuller",
+      "Leigh Kemp",
+      "Avleen Dawson",
+      "Sammy Bonilla",
+      "Acacia Becker",
+      "Coral Shepherd",
+      "Melina Molina",
+      "Kiran Bailey",
+      "Clara Escobar",
+      "Alexandru Horn",
+      "Brandon-Lee Mercado",
+      "Elouise Weston",
+      "King Long",
+      "Kerri Searle",
+      "Kanye Hamer",
+      "Elwood Benitez",
+      "Mikail Whitaker",
+      "Bobby Hardy",
+      "Talha Ferry",
+      "Priscilla Landry",
+      "Olivia-Grace Cain",
+      "Kiaan Wallace",
+      "Wesley Padilla90",
+      "Ella-Grace Wooten91",
+      "Kaif Molloy92",
+      "Kamal Broadhurst93",
+      "Bianca Ferrell94",
+      "Micheal Talbot95",
+    ],
+  };
 
-    return (
-        <div className="App">
-            <Tasks data={data1}/>
-            <Tasks data={data2}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Tasks data={data1} />
+      <Tasks data={data2} />
+    </div>
+  );
 }
 
 export default App;
-
 
 //--------------------------------------------------------------
 // import React from 'react';
--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import {DataType} from './App';
+import React from "react";
+import { DataType } from "./App";
 
 type TasksType = {
-    data: DataType
-}
-
-export const Tasks = (props: TasksType) => {
-    return (
-        <div>
-            <h3>{props.data.title}</h3>
-            <ul>
-                {props.data.tasks.map(el => {
-                    return (
-                        <li><span>{el.title}</span><input type="checkbox" checked={el.isDone}/></li>
-                    );
-                })}
-            </ul>
-            <ul>
-                {props.data.students.map(el => {
-                    return (
-                        <li>{el}</li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  data: DataType;
 };
 
+export const Tasks = (props: TasksType) => {
+  return (
+    <div>
+      <h3>{props.data.title}</h3>
+      <ul>
+        {props.data.tasks.map((el) => {
+          return (
+            <li>
+              <span>{el.title}</span>
+              <input type="checkbox" checked={el.isDone} />
+            </li>
+          );
+        })}
+      </ul>
+      <ul>
+        {props.data.students.map((el) => {
+          return <li>{el}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
