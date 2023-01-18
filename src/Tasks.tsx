01bf315156aa043cb980db4cd0671f441778a1ae
tamga05 <tamga05@gmail.com>
2023-01-18 16:02:20 +0300

    coding and typing training

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,29 +1,31 @@
-import React from "react";
-import { DataType } from "./App";
+import React from 'react';
+import {DataType} from './App';
 
 type TasksType = {
-  data: DataType;
-};
+    data: DataType
+}
 
 export const Tasks = (props: TasksType) => {
-  return (
-    <div>
-      <h3>{props.data.title}</h3>
-      <ul>
-        {props.data.tasks.map((el) => {
-          return (
-            <li>
-              <span>{el.title}</span>
-              <input type="checkbox" checked={el.isDone} />
-            </li>
-          );
-        })}
-      </ul>
-      <ul>
-        {props.data.students.map((el) => {
-          return <li>{el}</li>;
-        })}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h3>{props.data.title}</h3>
+            <ul>
+                {props.data.tasks.map(el => {
+                    return (
+                        <li><span>{el.title}</span><input type="checkbox" checked={el.isDone}/></li>
+                    );
+                })}
+            </ul>
+            <ul>
+                <li>
+                    {props.data.students.map(el => {
+                        return (
+                            <li>{el}</li>
+                        );
+                    })}
+                </li>
+            </ul>
+        </div>
+    );
 };
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("root") as HTMLElement;
+const container  = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(<App />);
 
@@ -12,3 +12,4 @@ root.render(<App />);
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
