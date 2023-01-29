bf3beca660011a38b283a754a30388a8b25ca7a9
tamga05 <tamga05@gmail.com>
2023-01-29 15:24:38 +0300

    coding and typing training

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -4,25 +4,26 @@ import {DataType} from './App';
 type TasksType = {
     data: DataType
 }
+
 export const Tasks = (props: TasksType) => {
     return (
         <div>
             <h3>{props.data.title}</h3>
             <ul>
+                {props.data.tasks.map(el => {
+                    return (
+                        <li><span>{el.title}</span><input type="checkbox" checked={el.isDone}/></li>
+                    );
+                })}
+            </ul>
+            <ul>
                 {
-                    props.data.tasks.map(el => {
+                    props.data.students.map(el => {
                         return (
-                            <li><span>{el.title}</span>{el.isDone}</li>
+                            <li>{el}</li>
                         );
                     })
                 }
-            </ul>
-            <ul>
-                {props.data.students.map(el => {
-                    return (
-                        <li>{el}</li>
-                    );
-                })}
             </ul>
         </div>
     );
